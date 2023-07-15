f2b34e4a35ab5aa47310733509777829c97af2f3
“Jhonny <aniceto.0760@gmail.com>
2023-07-15 16:47:11 -0400

    Arreglo HTML de contacto

---
+++ src/app/models/CosturaAng.ts
@@ -0,0 +1,18 @@
+export class CosturaAng {
+    id?: string;
+    nombre: string;
+    apellido: string;
+    email: string;
+    mobil: number;
+
+     constructor(nombre: string, apellido: string, email: string, mobil: number) {
+
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.mobil = mobil;
+
+     }
+
+
+}
