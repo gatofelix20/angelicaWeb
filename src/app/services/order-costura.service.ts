ef9c90c68138a6287d9c46b33d787f9fb6e00aaa
“Jhonny <aniceto.0760@gmail.com>
2023-09-19 17:38:14 -0400

    Edite las images de cost

--- src/app/services/order-costura.service.ts
+++ src/app/services/order-costura.service.ts
@@ -51,35 +51,35 @@ foodDetails = [
     foodName:"Vestido de Fiesta",
     foodDetails:"Es un  vestido con escote tipo v confeccionado en tafeta de seda en color champagne y rebordado en chantilly color hueso con pedrería la falda es circular completa en gasa blanca.",
     foodPrice:200,
-    foodImg:"assets/img/cost-12.jpg"
+    foodImg:"assets/img/cost-1.jpeg"
   },
   {
     id:2,
     foodName:"Vestido Dharma",
     foodDetails:"El traje está realizado en tela de organza natural, en su parte superior presenta un un escote trapezoidal y en el hombro izquierdo tiene aplicaciones realizadas en pétalos de rosa, va ceñido hasta la cadera desde donde se desprende una falta con caída amplia y cola.",
     foodPrice:369,
-    foodImg:"assets/img/cost-13.png"
+    foodImg:"assets/img/cost-2.jpeg"
   },
   {
     id:3,
     foodName:"Vestido de Noche",
     foodDetails:"Es un vestido largo ceñido al cuerpo hasta la altura de las rodillas, es en forma de strapless, elaborado en tafetán tornasolado, con detalles y aplicaciones en el costado izquierdo. La falda que se desprende tiene vuelo y termina en cola.",
     foodPrice:149,
-    foodImg:"assets/img/cost-3.jpeg"
+    foodImg:"assets/img/cost-3.jpg"
   },
   {
     id:4,
     foodName:"Vestido Dhrama Elegante",
     foodDetails:"Es un diseño en línea de sirena, confeccionado en tela de Chiffón de seda, enteramente recamado en vistosos acetatos tornasolados, presenta un escote de corte corazón y se complementa en la falda con una abertura lateral dándole un aire de feminidad y elegancia.",
     foodPrice:140,
-    foodImg:"assets/img/cost-4.jpeg"
+    foodImg:"assets/img/cost-4.png"
   },
   {
     id:5,
     foodName:"Vestido Galabodas",
     foodDetails:"Es un vestido “palabra de Honor”, del cuál se desprende una amplia falda elaborada en gasa de seda, en color amarillo infinito. La parte superior es en strapless con escote en forma de corazón en crepé satinado, en la cintura se detalla un falso cintillo decorado con canutillos mostacilla y en los mismos tonos del material en que está elaborado el vestido.",
     foodPrice:105,
-    foodImg:"assets/img/cost-5.jpeg"
+    foodImg:"assets/img/cost-5.jpg"
   },
   {
     id:6,
@@ -100,7 +100,7 @@ foodDetails = [
     foodName:"Traje de Baile",
     foodDetails:"Es un vestido largo o corto a las rodillas, con un hermoso estampado, algunos con armador y de seda. Por lo general, las mujeres lo utilizan con alpargatas y en la cabeza, un tocado de diferentes flores y colores. Se utiliza para bailar joropo, desfiles de moda, concursos de cantos, espectáculos llaneros, entre otros.",
     foodPrice:219,
-    foodImg:"assets/img/cost-11.jpeg"
+    foodImg:"assets/img/cost-8.jpg"
   }
 ]
 
